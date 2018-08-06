a2d2b56a93dee47726cfe76f61179bbda3d8315a
BasToni <a.visalli@fluidnext.com>
2018-08-06 14:24:29 +0200

    Fix sideline

--- app/plugin/sideline/src/SidelineResourceGenerator.js
+++ app/plugin/sideline/src/SidelineResourceGenerator.js
@@ -16,141 +16,139 @@ class SidelineResourceGenerator {
      * @param sideline
      * @param options
      */
-    generateResource(name, files, monitor, sideline, options = {autoFill:true}) {
+    generateResource(name, files, monitor, sideline, options = {}) {
 
-        let command = new this.ffmpeg();
-        let complexFilter = [];
-        let backgroundColor = options.backgroundColor ? options.backgroundColor : 'black';
-        complexFilter.push(`color=s=${monitor.width}x${monitor.height}:c=${backgroundColor} [base0]`);
-        // TODO CONTROLL
+        return new Promise((resolve, reject) => {
+            let command = new this.ffmpeg();
+            let complexFilter = [];
+            let backgroundColor = options.backgroundColor ? options.backgroundColor : 'black';
+            complexFilter.push(`color=s=${monitor.width}x${monitor.height}:c=${backgroundColor} [base0]`);
+            // TODO CONTROLL
 
-        // TODO Extrat to polygon of monitor
+            // TODO Extrat to polygon of monitor
 
-        let rowProgressWidth = 600;
-        let rowProgressHeight = sideline.height;
-        let index = 0;
-        let widthSideline = sideline.width;
 
-        while (widthSideline > 0) {
+            let rowProgressWidth = 660;
+            let rowProgressHeight = sideline.height;
+            let index = 0;
+            let widthSideline = sideline.width;
 
-            for (let cont = 0; cont < files.length; cont++) {
+            /**
+             * Add files
+             */
+            while (widthSideline > 0) {
+                for (let cont = 0; cont < files.length; cont++) {
 
-                rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+                    switch (true) {
+                        case monitor.width > (rowProgressWidth + files[cont].dimension.width) :
+                            complexFilter.push({
+                                filter: 'setpts=PTS-STARTPTS',
+                                inputs: `${index}:v`, outputs: `block${index}`
+                            });
+                            rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+                            index++;
+                            command = command.addInput(files[cont].location.path + files[cont].location.name);
+                            break;
+                        case monitor.width <= (rowProgressWidth + files[cont].dimension.width) :
+                            complexFilter.push({
+                                filter: 'setpts=PTS-STARTPTS',
+                                inputs: `${index}:v`, outputs: `block${index}`
+                            });
+                            index++;
+                            command = command.addInput(files[cont].location.path + files[cont].location.name);
 
-                switch (true) {
-                    case monitor.width > rowProgressWidth :
-                        command = command.addInput(files[cont].location.path + files[cont].location.name);
-                        complexFilter.push({
-                            filter: 'setpts=PTS-STARTPTS',
-                            inputs: `${index}:v`, outputs: `block${index}`
-                        });
-                        index++;
-                        widthSideline = widthSideline - files[cont].dimension.width;
-                        break;
-                    case monitor.width <= rowProgressWidth :
-                        command = command.addInput(files[cont].location.path + files[cont].location.name);
-                        complexFilter.push({
-                            filter: 'setpts=PTS-STARTPTS',
-                            inputs: `${index}:v`, outputs: `block${index}`
-                        });
-                        index++;
+                            rowProgressWidth = (rowProgressWidth + files[cont].dimension.width - monitor.width) - files[cont].dimension.width;
+                            rowProgressHeight = rowProgressHeight + sideline.height;
+                            complexFilter.push({
+                                filter: 'setpts=PTS-STARTPTS',
+                                inputs: `${index}:v`, outputs: `block${index}`
+                            });
+                            rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+                            index++;
+                            command = command.addInput(files[cont].location.path + files[cont].location.name);
+                            break;
+                    }
 
-                        command = command.addInput(files[cont].location.path + files[cont].location.name);
-                        complexFilter.push({
-                            filter: 'setpts=PTS-STARTPTS',
-                            inputs: `${index}:v`, outputs: `block${index}`
-                        });
-                        index++;
-                        rowProgressWidth = rowProgressWidth - monitor.width;
-                        widthSideline = widthSideline - files[cont].dimension.width;
-                        break;
+
+                    widthSideline = widthSideline - files[cont].dimension.width;
                 }
             }
-        }
 
-        let offsetX = 0;
-        rowProgressWidth = 600;
-        rowProgressHeight = 0;
-        index = 0;
-        widthSideline = sideline.width;
-        while (widthSideline > 0) {
 
-            for (let cont = 0; cont < files.length; cont++) {
+            widthSideline = sideline.width;
+            rowProgressWidth = 660;
+            rowProgressHeight = 0;
+            index = 0;
 
-                rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+            /**
+             * Calculate the coordinates of the files
+             */
+            while (widthSideline > 0) {
+                for (let cont = 0; cont < files.length; cont++) {
 
-                switch (true) {
-                    case monitor.width > rowProgressWidth :
-                        offsetX = rowProgressWidth - files[cont].dimension.width;
-                        console.group('MAX');
-                        console.log('INDEX', index);
-                        console.log('OFFESET', offsetX);
-                        console.log('ROW PROGRESS', rowProgressWidth);
-                        console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
-                        console.groupEnd();
-                        complexFilter.push({
-                            filter: 'overlay', options: { shortest:1, x: offsetX, y:  rowProgressHeight},
-                            inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
-                        });
-                        index++;
-                        widthSideline = widthSideline - files[cont].dimension.width;
-                        break;
-                    case monitor.width <= rowProgressWidth :
+                    switch (true) {
+                        case monitor.width > (rowProgressWidth + files[cont].dimension.width) :
+                            console.group('CONTAIN');
+                            console.log('INDEX', index);
+                            console.log('OFFESET', rowProgressWidth);
+                            console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
+                            console.groupEnd();
+                            complexFilter.push({
+                                filter: 'overlay', options: { shortest:1, x: rowProgressWidth, y:  rowProgressHeight},
+                                inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
+                            });
+                            rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+                            index++;
+                            break;
+                        case monitor.width <= (rowProgressWidth + files[cont].dimension.width) :
+                            console.group('NEWLINE 1');
+                            console.log('INDEX', index);
+                            console.log('OFFESET', rowProgressWidth);
+                            console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
+                            console.groupEnd();
+                            complexFilter.push({
+                                filter: 'overlay', options: { shortest:1, x: rowProgressWidth, y:  rowProgressHeight},
+                                inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
+                            });
+                            index++;
 
-                        offsetX = rowProgressWidth - files[cont].dimension.width;
-                        console.group('MIN 1');
-                        console.log('INDEX', index);
-                        console.log('OFFESET', offsetX);
-                        console.log('ROW PROGRESS', rowProgressWidth);
-                        console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
-                        console.groupEnd();
-                        complexFilter.push({
-                            filter: 'overlay', options: { shortest:1, x: offsetX, y:  rowProgressHeight},
-                            inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
-                        });
+                            rowProgressWidth = (rowProgressWidth + files[cont].dimension.width - monitor.width) - files[cont].dimension.width;
+                            rowProgressHeight = rowProgressHeight + sideline.height;
+                            console.group('NEWLINE 1');
+                            console.log('INDEX', index);
+                            console.log('OFFESET', rowProgressWidth);
+                            console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
+                            console.groupEnd();
+                            complexFilter.push({
+                                filter: 'overlay', options: { shortest:1, x: rowProgressWidth, y:  rowProgressHeight},
+                                inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
+                            });
+                            rowProgressWidth = rowProgressWidth + files[cont].dimension.width;
+                            index++;
+                            break;
+                    }
 
-                        index++;
-                        rowProgressHeight = rowProgressHeight + sideline.height;
-                        offsetX = monitor.width - (offsetX + files[cont].dimension.width);
-                        console.group('MIN 2');
-                        console.log('INDEX', index);
-                        console.log('OFFESET', offsetX);
-                        console.log('ROW PROGRESS WIDTH', rowProgressWidth);
-                        console.log('ROW PROGRESS HEIGHT', rowProgressHeight);
-                        console.groupEnd();
-                        complexFilter.push({
-                            filter: 'overlay', options: { shortest:1, x: rowProgressWidth - files[cont].dimension.width, y:  rowProgressHeight},
-                            inputs: [`base${index}`, `block${index}`], outputs: `base${index+1}`
-                        });
-
-                        index++;
-                        rowProgressWidth = rowProgressWidth - monitor.width;
-                        widthSideline = widthSideline - files[cont].dimension.width;
-                        break;
+                    widthSideline = widthSideline - files[cont].dimension.width;
                 }
             }
-        }
 
-        console.log(complexFilter);
+            console.log(complexFilter);
 
-        let outFile = 'test/out.mp4';
+            let outFile = 'test/out.mp4';
 
-        command
-            .complexFilter(complexFilter, 'base'+index)
-            .save(outFile)
-            .on('error', function(err) {
-                console.log('An error occurred: ' + err.message);
-            })
-            .on('progress', function(progress) {
-                console.log('... frames: ' + progress.frames);
-            })
-            .on('end', function() {
-                console.log('Finished processing');
-            });
+            command
+                .complexFilter(complexFilter, 'base'+index)
+                .save(outFile)
+                .on('error', function(err) {
+                    reject(err.message);
+                })
+                .on('progress', options.progress ? options.progress : () =>{})
+                .on('end', function(data) {
+                    resolve(data);
+                });
 
+        });
     }
-
-
 }
 
 module.exports = SidelineResourceGenerator;
